d3e1702002be9c4eff19e6a74f4176a24458ced5
Mark Mazur Lussenbug <Markcool3@gmail.com>
2021-11-03 16:12:48 +0100

    Create BookController.cs
    with addition of testable get method.

---
+++ src/LibraryCms/LibraryCms/Controllers/BookController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryCms.Controllers
+{
+    public class BookController : ControllerBase
+    {
+
+        private readonly LibraryCmsShared.Context _context = null;
+
+        public BookController()
+        {
+            _context = new LibraryCmsShared.Context();
+        }
+        
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult GetResponse()
+        {
+
+            string success = "Succesfully retrieved get response.";
+            var json = JsonConvert.SerializeObject(success, Formatting.Indented);
+            IActionResult response = Ok(json);
+            return response;
+        }
+
+    }
+}
