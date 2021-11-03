2e744d02994e167f18616a03470f073596a4d13b
Mark Mazur Lussenbug <Markcool3@gmail.com>
2021-11-03 15:27:41 +0100

    added usercontroller
    User controller added with simple add user from body method.

---
+++ src/LibraryCms/LibraryCms/Controllers/UserController.cs
@@ -0,0 +1,51 @@
+﻿using LibraryCmsShared.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryCms.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly LibraryCmsShared.Context _context = null;
+
+        public UserController()
+        {
+            _context = new LibraryCmsShared.Context();
+        }
+
+        [AllowAnonymous]
+        [HttpPost("action")]
+        public IActionResult AddUser([FromBody]User user)
+        {
+            try
+            {
+                _context.Users.Add(user);
+
+                user.IsAdmin = false;
+                user.ApprovedUser = false;
+                user.LoanLimit = 3;
+
+                _context.SaveChanges();
+
+                string success = "User created successfully";
+                var json = JsonConvert.SerializeObject(success, Formatting.Indented);
+                IActionResult response = Ok(json);
+                return response;
+            }
+            catch
+            {
+                string Failiure = "User already exists";
+                var json = JsonConvert.SerializeObject(Failiure, Formatting.Indented);
+                IActionResult response = BadRequest(json);
+                return response;
+            }
+        }
+    }
+}
