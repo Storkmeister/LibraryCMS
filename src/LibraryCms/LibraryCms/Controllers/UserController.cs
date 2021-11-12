2657d11ce3dfe360ac203e6030e9d9f57fb83faa
Mark Mazur Lussenbug <Markcool3@gmail.com>
2021-11-12 14:09:44 +0100

    approve user added

--- src/LibraryCms/LibraryCms/Controllers/UserController.cs
+++ src/LibraryCms/LibraryCms/Controllers/UserController.cs
@@ -170,6 +170,43 @@ namespace LibraryCms.Controllers
         }
 
         [Authorize]
+        [HttpPut]
+        public IActionResult ApproveUser([FromBody] User user)
+        {
+            var jwt = HttpContext.User.Identity as ClaimsIdentity;
+            IEnumerable<Claim> claim = jwt.Claims;
+            var NameId = claim.FirstOrDefault().Value;
+            if (NameId != null && _context.Users.Where(u => u.Id == Convert.ToInt32(NameId) && u.IsAdmin == true).SingleOrDefault() != null)
+            {
+                try
+                {
+                    _context.Users.Attach(user);
+                    user.ApprovedUser = true;
+                    var userEntry = _context.Entry(user);
+                    userEntry.Property("Id").IsModified = false;
+                    userEntry.State = EntityState.Modified;
+                    _context.SaveChanges();
+
+                    var json = JsonConvert.SerializeObject(user, Formatting.Indented);
+                    IActionResult response = Ok(json);
+                    return response;
+                }
+                catch
+                {
+                    var json = JsonConvert.SerializeObject(user, Formatting.Indented);
+                    IActionResult response = BadRequest(json);
+                    return response;
+                }
+
+            }
+            else
+            {
+                IActionResult response = BadRequest("You Need To Be Logged In To Do This Action.");
+                return response;
+            }
+        }
+
+        [Authorize]
         [HttpDelete]
         public IActionResult DeleteGenre([FromBody] int Id)
         {
