395f21db62e6a4d727eee537449550997ee09a0e
Mark Mazur Lussenbug <Markcool3@gmail.com>
2021-11-19 14:59:14 +0100

    git

--- src/librarycms/LibraryCms/Controllers/BookController.cs
+++ src/librarycms/LibraryCms/Controllers/BookController.cs
@@ -235,7 +235,7 @@ namespace LibraryCms.Controllers
             var jwt = HttpContext.User.Identity as ClaimsIdentity;
             IEnumerable<Claim> claim = jwt.Claims;
             var NameId = claim.FirstOrDefault().Value;
-            if (NameId != null && _context.Users.Where(u => u.Id.ToString() == NameId && u.IsAdmin == true).SingleOrDefault() != null)
+            if (NameId != null && AuthenticationController.IsAdminUser(NameId))
             {
                 try
                 {
@@ -313,7 +313,7 @@ namespace LibraryCms.Controllers
             var jwt = HttpContext.User.Identity as ClaimsIdentity;
             IEnumerable<Claim> claim = jwt.Claims;
             var NameId = claim.FirstOrDefault().Value;
-            if (NameId != null && _context.Users.Where(u => u.Id.ToString() == NameId && u.IsAdmin == true).SingleOrDefault() != null)
+            if (NameId != null && AuthenticationController.IsAdminUser(NameId))
             {
                 try
                 {
